a8eb1a8f215d15c3ed4609eb63c3f41c577a8cd0
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-24 12:06:25 -0500

    js solution for Part 2

--- js/romanNumeralsSpec.js
+++ js/romanNumeralsSpec.js
@@ -2,4 +2,4 @@ var rn = require("./romanNumerals");
 
 console.log(rn.toRoman(1) === "I");
 console.log(rn.toRoman(3) === "III");
-console.log(rn.toRoman(9) === "VIIII");
+console.log(rn.toRoman(4) === "IV");
