e64fcf5e8a362f87eb19fc36c6df515f882a1856
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-24 12:00:46 -0500

    js solution for Part 1

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,19 @@
-exports.toRoman = function(num) {
-
+exports.toRoman = function (num) {
+  let pattern = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1,
+  };
+  let result = "";
+  for (let Key in pattern) {
+    while (num >= pattern[Key]) {
+      result += Key;
+      num -= pattern[Key];
+    }
+  }
+  return result;
 };
--- js/romanNumeralsSpec.js
+++ js/romanNumeralsSpec.js
@@ -1,5 +1,5 @@
 var rn = require("./romanNumerals");
 
-console.log(rn.toRoman(1) === 'I');
-console.log(rn.toRoman(3) === 'III');
-console.log(rn.toRoman(4) === 'IV');
+console.log(rn.toRoman(1) === "I");
+console.log(rn.toRoman(3) === "III");
+console.log(rn.toRoman(9) === "VIIII");
