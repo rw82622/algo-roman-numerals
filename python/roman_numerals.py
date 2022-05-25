31ab002437caf115923a506a66c16bbc5526ee49
Rexford Wiafe <w.rexford@yahoo.com>
2022-05-25 11:24:39 -0500

    python solution for roman_numerals

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,22 @@
 def to_roman(num):
-    # write your code here!
+    pattern = {
+        'M': 1000,
+        'CM': 900,
+        'D': 500,
+        'CD': 400,
+        'C': 100,
+        'XC': 90,
+        'L': 50,
+        'XL': 40,
+        'X': 10,
+        'IX': 9,
+        'V': 5,
+        'IV': 4,
+        'I': 1,
+    }
+    result = ""
+    for Key in pattern:
+        while num >= pattern[Key]:
+            result += Key
+            num -= pattern[Key]
+    return result
